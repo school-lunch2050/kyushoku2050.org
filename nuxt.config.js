6b99a8cfe008725d22b7514bbf93551a613889e8
Martin Heidegger <martin.heidegger@gmail.com>
2021-02-16 16:26:59 +0900

    mobile sizing

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
     title: 'kyushoku2050.org',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' },
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
