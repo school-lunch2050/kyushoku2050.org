21a1a3bc58c34a94a17ab455c0a17be8b255d416
Martin Heidegger <martin.heidegger@gmail.com>
2021-02-17 03:44:50 +0900

    busing bubble text

---
+++ components/bubble-text.vue
@@ -0,0 +1,43 @@
+<template>
+  <g>
+    <svg-center-box
+      :x="rect.x"
+      :y="rect.y"
+      :width="rect.width"
+      :height="rect.height"
+      line-height="20"
+      :text="text"
+    />
+  </g>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    bubble: {
+      type: [String, Object],
+      required: true
+    },
+    viewRect: {
+      type: Object,
+      required: true
+    },
+    rect: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    text () {
+      const path = this.$props.id.split('.')
+      path.splice(1, 0, 'bubbles')
+      return this.$t(`weblate.${path.join('.')}.text`)
+    }
+  }
+})
+</script>
