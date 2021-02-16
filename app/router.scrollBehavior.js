5dbaf6f9bb6415802b88606626a7ea0e71720f30
Martin Heidegger <martin.heidegger@gmail.com>
2021-02-16 20:51:28 +0900

    pinning scroll position (will need to be adapted in future)

---
+++ app/router.scrollBehavior.js
@@ -0,0 +1,10 @@
+// https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-router#scrollbehavior
+export default function (/* to, from, savedPosition */) {
+  const [html] = document.getElementsByTagName('html')
+  const htmlPos = {
+    y: html.scrollTop,
+    x: html.scrollLeft
+  }
+  // console.log({ scroll: { to, from, savedPosition, htmlPos } })
+  return htmlPos
+}
