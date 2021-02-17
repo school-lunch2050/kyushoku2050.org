e7075a650cf19050e0adf5aa623325004aa11fd5
Martin Heidegger <martin.heidegger@gmail.com>
2021-02-17 13:07:54 +0900

    title for illustrations

--- components/lunch-detail.vue
+++ components/lunch-detail.vue
@@ -3,7 +3,17 @@
     <detail-header :type="type" />
     <main class="main--lunch-detail">
       <lunch :type="type" />
-      <slot />
+      <div ref="box" class="lunch-detail--zoom-box">
+        <svg
+          width="100vw"
+          height="100vh"
+          viewBox="0 0 3157 2500"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink">
+          <slot />
+        </svg>
+      </div>
     </main>
   </div>
 </template>
--- pages/desperate.vue
+++ pages/desperate.vue
@@ -2,51 +2,50 @@
   <!-- eslint-disable vue/max-attributes-per-line -->
   <div>
     <lunch-detail type="desperate">
-      <svg width="100%" height="100%" viewBox="0 0 3157 2500" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-        <image class="illustration--image" width="3157" height="2500" href="/img/illustration/desperate.webp" />
-        <bubble-text
-          id="desperate.mass_vegetables"
-          bubble="2376 349 2376 523 2042.521 523 2049.28125 589 1951.913 523 1482 523 1482 349"
-          :view-rect="{ x: 1430, y: 297, width: 1727, height: 857 }"
-          :rect="{ x: 1482, y: 347, width: 893, height: 177 }"
-        />
-        <bubble-text
-          id="desperate.insect_pollination"
-          bubble="2976 1163 2977 1403 2276 1403 2276 1386.371 2106.472656 1448 2275.999656 1287.191 2276 1163"
-          :view-rect="{ x: 1471, y: 864, width: 1612, height: 1026 }"
-          :rect="{ x: 2277, y: 1161.5, width: 702, height: 243 }"
-        />
-        <bubble-text
-          id="desperate.pesticide_resistant"
-          bubble="715 1578 715 1739 771.970703 1761 715 1788.35 715 1927 37 1927 37 1578"
-          :view-rect="{ x: 0, y: 1391, width: 1579, height: 584 }"
-          :rect="{ x: 36, y: 1574, width: 680, height: 354 }"
-        />
-        <bubble-text
-          id="desperate.weather_patterns"
-          bubble="541.712891 2180.425781 578.387 2298.999781 1332 2299 1332 2472 476 2472 476 2299 505.037 2298.999781"
-          :view-rect="{ x: 0, y: 1926.99999975, width: 1402, height: 573 }"
-          :rect="{ x: 476.429688, y: 2299.3125, width: 855, height: 173 }"
-        />
-        <bubble-text
-          id="desperate.printed_product"
-          bubble="3020 1563 3020 1918 2571.076 1918 2550.917969 1983.160156 2504.889 1918 2383 1918 2383 1563"
-          :view-rect="{ x: 2324, y: 1487, width: 833, height: 725 }"
-          :rect="{ x: 2383.4296875, y: 1563.3125, width: 637, height: 356 }"
-        />
-        <bubble-text
-          id="desperate.climate_change"
-          bubble="1366.5 322.5 1366.5 577 834.975 577 793.081055 633.991211 793.081 577 696 577 696 322.5"
-          :view-rect="{ x: 0, y: 236, width: 1438, height: 747 }"
-          :rect="{ x: 696, y: 322.3125, width: 671, height: 254 }"
-        />
-        <bubble-text
-          id="desperate.home_sprouts"
-          bubble="1349.4 2120.3750005 1349.4 2333.3 1880.839844 2333.3 1960.351563 2326.212891 1960.351563 2310.792969 2030.2 2292.710938 1965.480469 2260.3 1965.480469 2114.5"
-          :view-rect="{ x: 1308, y: 2079, width: 1849, height: 421 }"
-          :rect="{ x: 1349.429688, y: 2114.3125005, width: 617, height: 219 }"
-        />
-      </svg>
+      <image class="illustration--image" width="3157" height="2500" href="/img/illustration/desperate.webp" />
+      <svg-center-box x="550" y="40" width="1900" height="200" font-size="100" class="font--tex" fill="white" :text="$t('weblate.desperate.title')" />
+      <bubble-text
+        id="desperate.mass_vegetables"
+        bubble="2376 349 2376 523 2042.521 523 2049.28125 589 1951.913 523 1482 523 1482 349"
+        :view-rect="{ x: 1430, y: 297, width: 1727, height: 857 }"
+        :rect="{ x: 1482, y: 347, width: 893, height: 177 }"
+      />
+      <bubble-text
+        id="desperate.insect_pollination"
+        bubble="2976 1163 2977 1403 2276 1403 2276 1386.371 2106.472656 1448 2275.999656 1287.191 2276 1163"
+        :view-rect="{ x: 1471, y: 864, width: 1612, height: 1026 }"
+        :rect="{ x: 2277, y: 1161.5, width: 702, height: 243 }"
+      />
+      <bubble-text
+        id="desperate.pesticide_resistant"
+        bubble="715 1578 715 1739 771.970703 1761 715 1788.35 715 1927 37 1927 37 1578"
+        :view-rect="{ x: 0, y: 1391, width: 1579, height: 584 }"
+        :rect="{ x: 36, y: 1574, width: 680, height: 354 }"
+      />
+      <bubble-text
+        id="desperate.weather_patterns"
+        bubble="541.712891 2180.425781 578.387 2298.999781 1332 2299 1332 2472 476 2472 476 2299 505.037 2298.999781"
+        :view-rect="{ x: 0, y: 1926.99999975, width: 1402, height: 573 }"
+        :rect="{ x: 476.429688, y: 2299.3125, width: 855, height: 173 }"
+      />
+      <bubble-text
+        id="desperate.printed_product"
+        bubble="3020 1563 3020 1918 2571.076 1918 2550.917969 1983.160156 2504.889 1918 2383 1918 2383 1563"
+        :view-rect="{ x: 2324, y: 1487, width: 833, height: 725 }"
+        :rect="{ x: 2383.4296875, y: 1563.3125, width: 637, height: 356 }"
+      />
+      <bubble-text
+        id="desperate.climate_change"
+        bubble="1366.5 322.5 1366.5 577 834.975 577 793.081055 633.991211 793.081 577 696 577 696 322.5"
+        :view-rect="{ x: 0, y: 236, width: 1438, height: 747 }"
+        :rect="{ x: 696, y: 322.3125, width: 671, height: 254 }"
+      />
+      <bubble-text
+        id="desperate.home_sprouts"
+        bubble="1349.4 2120.3750005 1349.4 2333.3 1880.839844 2333.3 1960.351563 2326.212891 1960.351563 2310.792969 2030.2 2292.710938 1965.480469 2260.3 1965.480469 2114.5"
+        :view-rect="{ x: 1308, y: 2079, width: 1849, height: 421 }"
+        :rect="{ x: 1349.429688, y: 2114.3125005, width: 617, height: 219 }"
+      />
     </lunch-detail>
   </div>
 </template>
